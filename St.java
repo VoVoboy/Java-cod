c6cb1d0f16bd24732a22daa77cb5e93594ee714d
VoVoboy <korogodskijbogdan@gmail.com>
2016-12-05 23:59:19 +0200

    Add files via upload

---
+++ St.java
@@ -0,0 +1,12 @@
+package exp;
+
+/**
+ * Created by RogueBoy on 05.12.2016.
+ */
+public class St extends Eng{
+    public static void main(String args[]){
+        Eng e = new Eng();
+
+        e.Writer();
+    }
+}
