1ac99fa2a1b8bc0247d8294f92923fd3f69a22ed
VoVoboy <korogodskijbogdan@gmail.com>
2016-12-08 19:06:48 +0200

    boxing java with Switch

---
+++ AutoBoxSwitch.java
@@ -0,0 +1,19 @@
+package AutoBoxingJava;
+
+/**
+ * Created by RogueBoy on 08.12.2016.
+ */
+public class AutoBoxSwitch {
+    public static void main(String args[]) {
+
+        Integer iOb = 2;
+
+        switch (iOb) {
+            case 1: System.out.println("one");
+                break;
+            case 2: System.out.println("two");
+                break;
+            case 3: System.out.println("error");
+        }
+    }
+}
